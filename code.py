708ed87633c5c3b6603a6c32d9eefd3216013c00
Raeed Ahmed <ruahmed7868@gmail.com>
2021-11-14 22:31:28 -0800

    Read pins and attempt to send packet

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 import struct
 import time
 import usb_hid
+import board
+import digitalio
+from adafruit_debouncer import Debouncer
 
 
 class GamePad:
@@ -16,10 +19,21 @@ class GamePad:
                 break
             time.sleep(0.2)
         self.report = bytearray(2)
-        self.last_report = bytearray(2)
+        self.prev_report = bytearray(2)
+        self.button_states = 0
+        self.hat_pos = 0
 
     def update(self):
-        pass
+        struct.pack_into("<BB",  # format of bytes
+                         self.report,  # buffer to fill
+                         0,  # offset
+                         self.button_states,
+                         self.hat_pos  # data sources
+                         )
+        if self.prev_report != self.report:
+            print(self.report)
+            self.device.send_report(self.report, None)
+            self.prev_report[:] = self.report
 
     def info(self):
         print(f"Usage Page: {self.device.usage_page}",
@@ -28,6 +42,31 @@ class GamePad:
               )
 
 
+def create_button(pin) -> Debouncer:
+    button = digitalio.DigitalInOut(pin)
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    return Debouncer(button, 0)
+
+
 gamepad = GamePad()
 
 gamepad.info()
+
+button_pins = [getattr(board, f"GP{pin}") for pin in range(2, 13)]
+
+buttons = [create_button(pin) for pin in button_pins]
+
+clicks = 0
+while True:
+    for i, button in enumerate(buttons):
+        button.update()
+        if button.fell:
+            clicks += 1
+            print(f"Button {i} fell")
+            print(clicks)
+            gamepad.button_states |= 1 << i
+        if button.rose:
+            print(f"Button {i} rose")
+            gamepad.button_states &= ~(1 << i)
+        gamepad.update()
